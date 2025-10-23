1fbd0354228485fa062268f53bf1556d15be1f72
stivenescobarg <stivenescobar240208@gmail.com>
2025-10-23 15:03:48 -0500

    "Se implementó las soluciones sobre la declaración de variables, implementando int, double y String, asegurandose de que se impriman en la consola"

---
+++ demo/src/main/java/ejercicio1_2310/Main.java
@@ -0,0 +1,17 @@
+package ejercicio1_2310;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        System.out.println("El número es: " + numero);
+
+        double decimal = 5.99;
+        System.out.println("El número decimal es: " + decimal);
+
+        String texto = "Hola, Mundo!";
+        System.out.println(texto);
+
+    }
+}
